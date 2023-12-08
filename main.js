0bc609a0e20434ce715df8193941b67aef2bd341
Loki <tymonisat@gmail.com>
2023-12-08 21:51:27 +1300

    margin done

--- main.js
+++ main.js
@@ -20,7 +20,10 @@ renderer.render(scene, camera)
 
 // Torus
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 , wireframe: true});
+const donutTexture = new THREE.TextureLoader().load('imgs/donut.jpg');
+const material = new THREE.MeshStandardMaterial({ 
+  map: donutTexture
+});
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
