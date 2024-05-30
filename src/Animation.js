b4170785538691339c91b3941e44ac3d20782eb9
Loki <tymonisat@gmail.com>
2024-05-30 22:13:38 +1200

    renamed main to Animation, added test script

--- src/Animation.js
+++ src/Animation.js
@@ -49,19 +49,6 @@ scene.add(ambientLight)
 const controls = new OrbitControls(camera, renderer.domElement)
 
 
-// //moon
-// const moonTexture = new THREE.TextureLoader().load('imgs/moon.jpg');
-// const normalTexture = new THREE.TextureLoader().load('imgs/normal.jpg');
-// const moon = new THREE.Mesh(
-//   new THREE.SphereGeometry(3, 32, 32),
-//   new THREE.MeshStandardMaterial({ 
-//     map: moonTexture,
-//     normalMap: normalTexture
-//   })
-// );
-
-// scene.add(moon);
-
 //sun object
 const sun = new THREE.Mesh(
   new THREE.SphereGeometry(1, 32, 32),
